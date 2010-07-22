196f9d9f13248614570840ea8232f405f88f0e23
Justin Davis <juster@mclapdawg.local>
2010-07-22 12:38:13 -0700

    Remove trivial, unnecessary use of shared variable.

--- process.pl
+++ process.pl
@@ -3,8 +3,7 @@ use warnings;
 use threads;
 use threads::shared;
 use Time::HiRes qw/time/;
-
-my @thread_times : shared;
+use List::Util  qw(sum);
 
 sub workfunc {
     my $start = time();
@@ -14,27 +13,19 @@ sub workfunc {
         $init += ( ( $_ % 4 ) * ( $_ % 4 ) ) / ( $_ % 10 + 1 );
     }
     my $runtime = time() - $start;
-    { lock(@thread_times); push( @thread_times, $runtime ); }
     print "Thread " . threads::tid() . " :: stopped at " . localtime() . "\n";
     return $runtime;
 }
 
-# Setup
 my $num_threads = shift || 2;
 print "Running with $num_threads threads\n";
 
-# Main code
 my $master_start = time();
 my @threads =
   map { threads->create( \&workfunc, rand(10) ); } 1 .. $num_threads;
-my $thread_time = 0;
-for my $t (@threads) {
-    $thread_time += $t->join();
-}
+
+my $thread_time = sum map { $_->join() } @threads;
 my $master_time = time() - $master_start;
 
-# Find and print times
-my $lock_time = 0;
-{ lock(@thread_times); $lock_time += $_ for @thread_times; }
 print "Master logged $master_time runtime\n";
-print "Threads reported $lock_time combined runtime\n";
+print "Threads reported $thread_time combined runtime\n";
