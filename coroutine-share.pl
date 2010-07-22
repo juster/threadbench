ef722aae6580f4648cdf2834d6676be971b4c19f
Justin Davis <juster@mclapdawg.local>
2010-07-22 15:20:39 -0700

    Add coroutine data shared data benchmarks for perl and python.

---
+++ coroutine-share.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+
+use Time::HiRes qw(time);
+use List::Util  qw(sum);
+use Coro;
+
+my $counter = 0;
+
+sub workfunc {
+    my ($id, $init) = @_;
+
+    printf "Coroutine %d :: started at %s\n", $id, scalar localtime;
+    my $start = time;
+    for ( 1 .. 10_000_000 ) {
+        $init += ( $_ % 4 ) * ( $_ % 4 ) / ( $_ % 10 + 1 );
+        # Coro's are cooperative so we must cede to others but no locking
+        ++$counter;
+        cede;
+    }
+    printf "Coroutine %d :: stopped at %s\n", $id, scalar localtime;
+
+    terminate time - $start;
+}
+
+my $coro_count = shift || 2;
+print "Running with $coro_count coroutines\n";
+
+my $master_start = time;
+my @coros =
+  map { async \&workfunc, $_, rand 10 } 1 .. $coro_count;
+
+my $child_time  = sum map { $_->join() } @coros;
+my $master_time = time - $master_start;
+
+printf "Master logged %.3f runtime\n", $master_time;
+printf "Coroutines reported %.3f combined runtime\n", $child_time;
+print  "Shared counter equals $counter\n";
